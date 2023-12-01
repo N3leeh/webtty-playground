b88437ba4be160bf043894d56f6dabcc4883be61
Marcel Oed <marcel.oed@data-ng.com>
2023-12-01 15:11:25 +0100

    fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,13 @@ PLAYBOOK="webtty.yml"
 if ! command -v ansible &> /dev/null;then
   echo "ansible not found. Attempt to install."
   if dpkg -S /bin/ls >/dev/null 2>&1;then
-    sudo apt update && sudo apt install -y ansible python3-pip
-    pip3 install --user github3.py
+    apt update && apt install -y ansible
   elif rpm -q -f /bin/ls >/dev/null 2>&1;then
-    sudo yum install ansible
+    yum install ansible
   else
     echo "Don't know this package system (neither RPM nor DEB)."
     exit 1
   fi
 else
-  ansible 
+  ansible-playbook -i hosts webtty.yml
 fi
